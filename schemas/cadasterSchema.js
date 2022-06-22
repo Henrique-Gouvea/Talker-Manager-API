43bfa412cc1de52da067e0746cd513e265236e61
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-06-22 15:43:50 -0300

    refactor: fix cadasterSchema for test

--- schemas/cadasterSchema.js
+++ schemas/cadasterSchema.js
@@ -1,52 +1,46 @@
 const Joi = require('joi').extend(require('@joi/date'));
 
 const cadasterSchema = Joi.object({
-    name: Joi
-    .string()
-    .min(3)
-    .required()
+  name: Joi
+  .string()
+  .required()
+  .min(3)
     .messages({
-        'string.empty': 'O campo "name" é obrigatório',
-        'string.min': 'O "name" deve ter pelo menos 3 caracteres',
-        'any.required': 'O campo "name" é obrigatório',
-    }),
-    age: Joi
-    .number()
-    .min(18)
-    .integer()
-    .required()
+      'string.min': 'O "name" deve ter pelo menos 3 caracteres',
+      'any.required': 'O campo "name" é obrigatório',
+   }),
+  age: Joi
+  .number()
+  .required()
+  .min(18)
     .messages({
-        'number.empty': 'O campo "age" é obrigatório',
-        'number.min': 'A pessoa palestrante deve ser maior de idade',
-        'any.required': 'O campo "age" é obrigatório',
-    }),
-    talk: Joi.object({
-        watchedAt: Joi
-        .date()
-        .format('DD/MM/YYYY')
-        .required()
-        .messages({
-            'any.required': 'O campo "watchedAt" é obrigatório',
-            'date.format': 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
-        }),
-        rate: Joi
-        .number()
-        .required()
-        .min(1)
-        .max(5)
-        .message({
-            'number.empty': 'O campo "rate" é obrigatório',
-            'number.min': 'O campo "rate" deve ser um inteiro de 1 à 5',
-            'number.max': 'O campo "rate" deve ser um inteiro de 1 à 5',
-            'any.required': 'O campo "rate" é obrigatório',
-        }),
-    })
-    .required()
-    .messages({
-        'object.empty': 'O campo "talk" é obrigatório',
-        'any.required': 'O campo "talk" é obrigatório',
-        // 'object.rate': 'O campo "rate" é obrigatório',
-    }),
+      'any.required': 'O campo "age" é obrigatório',
+      'number.min': 'A pessoa palestrante deve ser maior de idade',
+ }),
+ talk: Joi.object({
+  watchedAt: Joi
+  .date()
+  .format('DD/MM/YYYY')
+  .required()
+  .messages({
+    'any.required': 'O campo "watchedAt" é obrigatório',
+    'date.format': 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+  }),
+  rate: Joi
+  .number()
+  .required()
+  .min(1)
+  .max(5)
+  .messages({
+    'any.required': 'O campo "rate" é obrigatório',
+    'number.min': 'O campo "rate" deve ser um inteiro de 1 à 5',
+    'number.max': 'O campo "rate" deve ser um inteiro de 1 à 5',
+
+  }),
+ }).required()
+ .messages({
+  'any.required': 'O campo "talk" é obrigatório',
+ }),
 }).required();
 
 module.exports = cadasterSchema;
