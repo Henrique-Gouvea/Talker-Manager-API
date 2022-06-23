469982cc7f1cfc880d37911b958dad1903c8ae96
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-06-23 08:52:22 -0300

    refactor: index.js : remove comments

--- index.js
+++ index.js
@@ -16,20 +16,6 @@ app.get('/', (_request, response) => {
 app.use('/talker', talkersRouter);
 app.use('/login', loginRouter);
 
-// app.get('/talker/search', middlewares.authToken, controllers.searchTalker);
-
-// app.get('/talker', controllers.getAll);
-
-// app.get('/talker/:id', controllers.getById);
-
-// app.post('/login', middlewares.authLogin, controllers.getToken);
-
-// app.post('/talker', middlewares.authCadaster, controllers.createTalker);
-
-// app.put('/talker/:id', middlewares.authCadaster, controllers.updateTalker);
-
-// app.delete('/talker/:id', middlewares.authToken, controllers.deleteTalker);
-
 app.listen(PORT, () => {
   console.log('Online');
 });
